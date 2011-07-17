fbc557ad3494f3d3ed3bc9628719545afdef5eca
Alex Objelean <alex.objelean@gmail.com>
2011-07-17 18:41:11 +0300

    moving javascript resources to src/test/javascript & add src/main/javascript to build resources list

--- src/main/resources/archetype-resources/src/test/javascript/hello-spec.js
+++ src/main/resources/archetype-resources/src/test/javascript/hello-spec.js
@@ -3,3 +3,9 @@ describe('hello',function(){
     expect(window.hello).toBe("Hello There, ");
   });
 });
+
+describe('testAlex',function() {
+  it('it message',function() {
+    expect(window.hello).toBe("Hello There1, ");
+  });
+});
